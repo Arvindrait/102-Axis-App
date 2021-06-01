d86ef2accb716e98ad298ab40a5c8db4c28ef9ca
Arvindra <tech.arvindra720@gmail.com>
2021-06-01 13:00:27 +0530

    added init() method

--- DateUtils.java
+++ DateUtils.java
@@ -3,7 +3,14 @@ public class DateUtils
 
    int a=10;  
    int b=20;
-	public static void main(String[] args) 
+
+   public void init(){
+
+    
+     //logic
+
+     }
+ public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
 	}
