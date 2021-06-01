a9a65e756df7c2c531700087bcf0592f5017f358
Arvindrait <85031670+Arvindrait@users.noreply.github.com>
2021-06-01 12:56:53 +0530

    Update DateUtils.java

--- DateUtils.java
+++ DateUtils.java
@@ -3,8 +3,14 @@ public class DateUtils
 
    int a=10;  
    int b=20;
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
-	}
+	
+ public void format(){
+ 
+ 
+ //logic
+ }	
+public static void main(String[] args) 
+{
+ System.out.println("Hello World!");
+ }
 }
